0515f7716c797dc782e2212724cfdf45edb9e3d7
Petr Pichugin <petr.pichugin@gmail.com>
2023-01-03 15:34:45 +0500

    Problem 6 solved

---
+++ Problem_006/sumSquareDiff.hs
@@ -0,0 +1,7 @@
+module ProjectEuler where
+
+sumSquareDiff10 :: Int
+sumSquareDiff10 = sum [1..10]^2 - sum [n^2 | n <- [1..10]]
+
+sumSquareDiff100 :: Int
+sumSquareDiff100 = sum [1..100]^2 - sum [n^2 | n <- [1..100]]
