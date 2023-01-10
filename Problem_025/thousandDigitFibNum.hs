980068566ad9fc9e0e3167a0e9e095605ffd8cba
Petr Pichugin <petr.pichugin@gmail.com>
2023-01-10 12:41:45 +0500

    solution has been optimized. Now it takes 0.5 sec to find the index

--- Problem_025/thousandDigitFibNum.hs
+++ Problem_025/thousandDigitFibNum.hs
@@ -1,19 +1,13 @@
 module ProjectEuler where
 
 main :: IO ()
-main = do 
-    let x = find1000digitFibonacci
+main = do
+    let x = findIdxByndigits' 1000 -- take 0.59 sec to find
     print x
 
-find1000digitFibonacci :: [(Integer, Integer)]
-find1000digitFibonacci = take 1 [(y,n) | n <- [1..], let y = fibonacci n, length (show y) == 1000]
-
-fibonacci :: Integer -> Integer
-fibonacci n = let 
-   helper :: Integer -> Integer -> Integer -> Integer
-   helper 0 f1 _ = f1
-   helper n f1 f2 
-    | n == 0 = f1
-    | n > 0 = helper (n - 1) f2 (f2 + f1)
-    | n < 0 = helper (n + 1) f2 (f1 - f2)
-  in helper n 0 1
+findIdxByndigits' :: Num t => Int -> t
+findIdxByndigits' y = let
+        helper n f1 f2
+          | length (show f1) == y = n
+          | otherwise = helper (n + 1) f2 (f2 + f1)
+       in helper 0 0 1
