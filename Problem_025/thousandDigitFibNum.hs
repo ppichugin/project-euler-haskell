95dfbbf467c963e8e24277229dc4df307a336781
Petr Pichugin <petr.pichugin@gmail.com>
2023-01-10 12:53:41 +0500

    add README

--- Problem_025/thousandDigitFibNum.hs
+++ Problem_025/thousandDigitFibNum.hs
@@ -2,11 +2,11 @@ module ProjectEuler where
 
 main :: IO ()
 main = do
-    let x = findIdxByndigits' 1000 -- take 0.59 sec to find
+    let x = findIdxFstTermNDigitsNum 1000 -- take 0.59 sec to find
     print x
 
-findIdxByndigits' :: Num t => Int -> t
-findIdxByndigits' y = let
+findIdxFstTermNDigitsNum :: Num t => Int -> t
+findIdxFstTermNDigitsNum y = let
         helper n f1 f2
           | length (show f1) == y = n
           | otherwise = helper (n + 1) f2 (f2 + f1)
